8b248293eb09a598fc01e40edee8efc4305059cc
919042984@qq.com <chenjinxi199210>
2017-06-03 22:51:15 +0800

    暂存

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,21 @@
+
+//登录接口
+function login() {
+    $.ajax({
+        url: url + "login/login",
+        data: {keyword:$("#keyword").val(),password:$("#password").val()},
+        type: 'post',
+        success: function (result) {
+            if (result) {
+                alert('登录成功');
+                window.location.href = url + "index.html"
+            } else {
+                alert('登录失败');
+            }
+        }
+    });
+}
+
+$("#login").on("click",function () {
+    login();
+})
